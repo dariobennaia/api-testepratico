8170f3350d34fed0b5e990decb5bd82937ffa626
Dário Santos <dariobennaia@gmail.com>
2019-05-30 16:57:05 -0300

    Criação da migration para tabela litragens refrigerantes

---
+++ database/migrations/2019_05_30_192840_create_litragens_refrigerantes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLitragensRefrigerantes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('litragens_refrigerantes', function (Blueprint $table) {
+            $table->increments('id_litragem_refrigerante');
+            $table->string('litragem_refrigerante', 20);
+            $table->boolean('flg_ativo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('litragens_refrigerantes');
+    }
+}
