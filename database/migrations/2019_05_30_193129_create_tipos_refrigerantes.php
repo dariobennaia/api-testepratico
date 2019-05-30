de61ec3adc7b3ebc56bd0701cb1bcb3278b09b7e
Dário Santos <dariobennaia@gmail.com>
2019-05-30 16:57:34 -0300

    Criação da migration para tabela tipos refrigerantes

---
+++ database/migrations/2019_05_30_193129_create_tipos_refrigerantes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTiposRefrigerantes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipos_refrigerantes', function (Blueprint $table) {
+            $table->increments('id_tipo_refrigerante');
+            $table->string('tipo_refrigerante', 20);
+            $table->boolean('flg_ativo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipos_refrigerantes');
+    }
+}
